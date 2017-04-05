3464898ec80a63b492494ce6adb99e124834046c
kdbz <kinyuawallace@gmail.com>
2017-04-05 13:38:38 +0300

    Solution to class one exercise

---
+++ src/LunchTime.java
@@ -0,0 +1,21 @@
+/**
+* Our first java program
+* === Access modifiers
+* ====== public, protected,private
+*
+* ------------------------------
+* --> Inorder to run java on the command line, do the following
+*   (a) compile the java source file
+*          javac LunchTime.java
+*   (b) run the compiled java class
+*          java LunchTime
+*            (NB : remember the error that we had in class? What we're we doing wrong?)
+*
+*/
+public class LunchTime{
+
+  public static void main(String args []){
+    System.out.println("It's lunch time!");
+  }
+
+}
